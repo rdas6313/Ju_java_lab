7ec7353bf13338b1f60eecaecf22283b05bdd3bd
Raja <rdas6313@gmail.com>
2024-03-25 13:39:23 +0530

    Solved question 2

---
+++ assignment_1/q2.java
@@ -0,0 +1,8 @@
+class q2{
+	public static void main(String args[]){
+		System.out.println("Total arguments: "+args.length);
+		for(int i=0;i<args.length;i++){
+			System.out.println("Args["+i+"] = " + args[i]);
+		}
+	}
+}
