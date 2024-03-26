5fa6b57df0c09ba610e8bd6fc2b3c7054329867d
Raja Das <rdas6313@gmail.com>
2024-03-26 23:58:33 +0530

    solved question 6

---
+++ assignment_1/q6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class q6{
+	public static void show(int data){
+		System.out.println(data);
+	}
+	public static void show(double data){
+		System.out.println(data);
+	}
+	public static void main(String args[]){
+		//Scanner scan = new Scanner(System.in);
+		short a = 20;
+		double d = 20.5;
+		show(a);
+		show(d);		
+	}
+}
